89b9fe8442e63c69ee64ea7f8fdbe80f71f23d67
Alyssa Resh <alyssaresh@icloud.com>
2025-01-15 21:11:32 -0500

    for demo day

--- src/App.jsx
+++ src/App.jsx
@@ -62,11 +62,10 @@ const [selectedMovieId, setSelectedMovieId] = useState("");
   };
 
 
-  const movies = [
+const movies = [
             { contentId: 1,
-              title: 'Die Hard',
+              //title: 'Die Hard',
               genre: 'Action',
-              genre: 'Drama', 
               rating: 'R',
               cast: 'Bruce Willis, Alan Rickman',
               releaseYear: '1988',
@@ -76,7 +75,7 @@ const [selectedMovieId, setSelectedMovieId] = useState("");
               videoUrl: '/Trailers/Die Hard.mp4'
             },
             { contentId: 2,
-              title: '30 Days of Night',
+              //title: '30 Days of Night',
               genre: 'Horror',
               rating: 'R',
               cast: 'Josh Hartnett, Danny Huston',
@@ -87,7 +86,7 @@ const [selectedMovieId, setSelectedMovieId] = useState("");
               videoUrl: '/Trailers/30 Days of Night.mp4'
             },
             { contentId: 3,
-              title: 'Back to the Future',
+              //title: 'Back to the Future',
               genre: 'Sci-fi',
               rating: 'PG',
               cast: 'Michael J. Fox, Christopher Lloyd',
@@ -98,7 +97,7 @@ const [selectedMovieId, setSelectedMovieId] = useState("");
               videoUrl: '/Trailers/Back to the Future.mp4'
             },
             { contentId: 4,
-              title: 'the Iron Giant', 
+              //title: 'the Iron Giant',
               genre: 'Animation',
               rating: 'PG',
               cast: 'Vin Diesel, Jennifer Aniston',
@@ -109,7 +108,7 @@ const [selectedMovieId, setSelectedMovieId] = useState("");
               videoUrl: '/Trailers/the Iron Giant.mp4'
             },
             { contentId: 5,
-              title: 'Interstellar',
+              //title: 'Interstellar',
               genre: 'Sci-fi',
               rating: 'PG-13',
               cast: 'Mathew McConaughey, Anne Hathaway',
@@ -117,10 +116,10 @@ const [selectedMovieId, setSelectedMovieId] = useState("");
               length: '169 minutes',
               description: 'When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.',
               thumbnail: '/video-thumbnails/Interstellar.png',
-              videoUrl: '/Trailers/Interstellar.mp4' 
+              videoUrl: '/Trailers/Interstellar.mp4'
             },
             { contentId: 6,
-              title: 'Toy Story',
+              //title: 'Toy Story',
               genre: 'Animation',
               rating: 'G',
               cast: 'Tom Hanks, Tim Allen',
@@ -131,7 +130,7 @@ const [selectedMovieId, setSelectedMovieId] = useState("");
               videoUrl: '/Trailers/Toy Story.mp4'
             },
             { contentId: 7,
-              title: 'Toy Story 2',
+              //title: 'Toy Story 2',
               genre: 'Animation',
               rating: 'G',
               cast: 'Tom Hanks, Tim Allen',
@@ -140,6 +139,105 @@ const [selectedMovieId, setSelectedMovieId] = useState("");
               description: 'When Woody is stolen by a toy collector, Buzz and his friends set out on a rescue mission to save Woody before he becomes a museum toy property with his roundup gang Jessie, Prospector, and Bullseye.',
               thumbnail: '/video-thumbnails/Toy Story 2.png',
               videoUrl: '/Trailers/Toy Story 2.mp4'
+            },
+            { contentId: 8,
+              //title: 'Toy Story 3',
+              genre: 'Animation',
+              rating: 'G',
+              cast: 'Tom Hanks, Tim Allen',
+              releaseDate: '2010',
+              length: '103 minutes',
+              description: 'The toys are mistakenly delivered to a day-care center instead of the attic right before Andy leaves for college, and it;s up to Woody to convince the other toys that they weren’t abandoned and to return home.',
+              thumbnail: '/video-thumbnails/Toy Story 3.png',
+              videoUrl: '/Trailers/the Iron Giant.mp4'
+            },
+            { contentId: 9,
+              //title: 'Lord of the Rings',
+              genre: 'Action',
+              rating: 'PG-13',
+              cast: 'Elijah Wood, Ian Mckellen',
+              releaseDate: '2001',
+              length: '178 minutes',
+              description: 'A meek Hobbit from the shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.',
+              thumbnail: '/video-thumbnails/Lord of the Rings.png',
+              videoUrl: '/Trailers/the Iron Giant.mp4'
+            },
+            { contentId: 10,
+              //title: 'Lord of the Rings 2',
+              genre: 'Action',
+              rating: 'PG-13',
+              cast: 'Elijah Wood, Ian Mckellen',
+              releaseDate: '2002',
+              length: '179 minutes',
+              description: 'While Frodo and Sam edge closer to Mordor with the help of the shifty Gollum, the divided fellowship makes a stand against Sauron’s new ally, Saruman, and his hordes of Isengard. ',
+              thumbnail: '/video-thumbnails/Lord of the Rings 2.png',
+              videoUrl: '/Trailers/the Iron Giant.mp4'
+            },
+            { contentId: 11,
+              //title: 'Lord of the Rings 3',
+              genre: 'Action',
+              rating: 'PG-13',
+              cast: 'Elijah Wood, Ian Mckellen',
+              releaseDate: '2003',
+              length: '201 minutes',
+              description: 'Gandalf and Aragorn lead the World of Men against Sauron’s army to draw his gaze from Froso and Sam as they approach Mount Doom with the One Ring.',
+              thumbnail: '/video-thumbnails/Lord of the Rings 3.png',
+              videoUrl: '/Trailers/the Iron Giant.mp4'
+            },
+            { contentId: 12,
+              //title: 'Small Soldiers',
+              genre: 'Action',
+              rating: 'PG-13',
+              cast: 'Kirsten Dunst, Tommy Lee Jones',
+              releaseDate: '1998',
+              length: '108 minutes',
+              description: 'When missile technology is used to enhance toy action figures, the toys soon begin to take their battle programming too seriously.',
+              thumbnail: '/video-thumbnails/Small Soldiers.png',
+              videoUrl: '/Trailers/the Iron Giant.mp4'
+            },
+            { contentId: 13,
+              //title: 'the Prince of Egypt',
+              genre: 'Animation',
+              rating: 'PG',
+              cast: 'Val Kilmer, Ralph Fiennes',
+              releaseDate: '1998',
+              length: '99 minutes',
+              description: 'Egyptian Prince Moses learns of his identity as a Hebrew and his destiny to become the chosen deliverer of his people.',
+              thumbnail: '/video-thumbnails/The Prince Of Egypt.png',
+              videoUrl: '/Trailers/the Iron Giant.mp4'
+            },
+            { contentId: 14,
+              //title: 'The Thing',
+              genre: 'Sci-fi',
+              rating: 'R',
+              cast: 'Kurt Russell, Keith David',
+              releaseDate: '1982',
+              length: '109 minutes',
+              description: 'A research team in Antarctica is hunted by a shape-shifting alien that assumes the appearance of its victims.',
+              thumbnail: '/video-thumbnails/Thing.png',
+              videoUrl: '/Trailers/the Iron Giant.mp4'
+            },
+            { contentId: 15,
+              //title: 'Everything Everywhere All at Once',
+              genre: 'Sci-fi',
+              rating: 'R',
+              cast: 'Michelle Yeoh, Ke Huy Quan',
+              releaseDate: '2022',
+              length: '139 minutes',
+              description: 'A middle-aged Chinese immigrant is swept up into an insane adventure in which she alone can save existence by exploring other universes and connecting with the lives she could have led.',
+              thumbnail: '/video-thumbnails/Everything Everywhere All At Once.png',
+              videoUrl: '/Trailers/the Iron Giant.mp4'
+            },
+            { contentId: 16,
+              //title: 'Unbreakable',
+              genre: 'Sci-fi',
+              rating: 'PG-13',
+              cast: 'Bruce Willis, Samuel L. Jackson',
+              releaseDate: '2000',
+              length: '106 minutes',
+              description: 'A man learns something extraordinary about himself after a devastating accident.',
+              thumbnail: '/video-thumbnails/Unbreakable.png',
+              videoUrl: '/Trailers/the Iron Giant.mp4'
             },
                   ];
 
@@ -241,8 +339,105 @@ const tvShows = [
     videoUrl: '/Trailers/Chernobyl.mp4',
     description: 'In April 1986, the city of Chernobyl in the Soviet Union suffers one of the worst nuclear disasters in the history of mankind. Consequently, many heroes put their lives on the line in the following days, weeks and months.'
   },
- 
+  {
+    contentId: 9,
+    title: 'Modern Family',
+    genre: 'Comedy',
+    rating: 'TV-PG',
+    cast: 'Ty Burrell, Sofia Vergara',
+    releaseYear: '2009',
+    seasons: '11',
+    thumbnail: '/video-thumbnails/Modern Family.png',
+    videoUrl: '/Trailers/',
+    description: 'Three different, but related, families face trials and tribulations in their own uniquely comedic ways.'
+  },
+  {
+    contentId: 10,
+    title: 'Community',
+    genre: 'Comedy',
+    rating: 'TV-14',
+    cast: 'Joel McHale, Donald Glover',
+    releaseYear: '2009',
+    seasons: '6',
+    thumbnail: '/video-thumbnails/Community.png',
+    videoUrl: '/Trailers/',
+    description: 'A suspended lawyer is forced to enroll in a community college with an eccentric staff and student body.'
+  },
+  {
+    contentId: 11,
+    title: 'The Simpsons',
+    genre: 'Comedy',
+    rating: 'TV-14',
+    cast: 'Dan Castellaneta, Nancy Cartwright',
+    releaseYear: '1989',
+    seasons: '32',
+    thumbnail: '/video-thumbnails/The Simpsons.png',
+    videoUrl: '/Trailers/',
+    description: 'The satiric half-hour adventures of a working-class family in the misfit city of Springfield.'
+  },
+  {
+    contentId: 12,
+    title: 'Stranger Things',
+    genre: 'Sci-fi',
+    rating: 'TV-14',
+    cast: 'Winona Ryder, Millie Bobby Brown',
+    releaseYear: '2016',
+    seasons: '4',
+    thumbnail: '/video-thumbnails/Stranger Things.png',
+    videoUrl: '/Trailers/',
+    description: 'In 1980s Indiana, a group of young friends witness supernatural forces and secret government exploits. As they search for answers, the children unravel a series of extraordinary mysteries. '
+  },
+  {
+    contentId: 13,
+    title: 'The Mandalorian',
+    genre: 'Sci-fi',
+    rating: 'TV-MA',
+    cast: 'Pedro Pascal, Chris Bartlett',
+    releaseYear: '2019',
+    seasons: '3',
+    thumbnail: '/video-thumbnails/The Mandalorian.png',
+    videoUrl: '/Trailers/',
+    description: 'The travels of a lone bounty hunter in the outer reaches of the galaxy, far from the authority of the New Republic.'
+  },
+  {
+    contentId: 14,
+    title: 'X-Files',
+    genre: 'Sci-fi',
+    rating: 'TV-14',
+    cast: 'David Duchovny, Gillian Anderson',
+    releaseYear: '1993',
+    seasons: '11',
+    thumbnail: '/video-thumbnails/The X-Files.png',
+    videoUrl: '/Trailers/',
+    description: 'Two F.B.I. Agents, Fox Mulder the believer and Dana Scully the skeptic, investigate the strange and unexplained, while hidden forces work to impede their efforts.'
+  },
+  {
+    contentId: 15,
+    title: 'Star Trek',
+    genre: 'Sci-fi',
+    rating: 'TV-PG',
+    cast: 'William Shatner, Leonard Nimoy',
+    releaseYear: '1996',
+    seasons: '3',
+    thumbnail: '/video-thumbnails/Star Trek.png',
+    videoUrl: '/Trailers/',
+    description: 'In the 23rd Century, Captain James T. Kirk and the crew of the U.S.S. Enterprise explore the galaxy and defend the united Federation of Planets.'
+  },
+  {
+    contentId: 16,
+    title: 'Firefly',
+    genre: 'Sci-fi',
+    rating: 'TV-14',
+    cast: 'Nathan Fillion, Gina Torres',
+    releaseYear: '2002',
+    seasons: '1',
+    thumbnail: '/video-thumbnails/Firefly.png',
+    videoUrl: '/Trailers/',
+    description: 'Five hundred years in the future, a renegade crew aboard a small spacecraft tries to survive as they travel the unknown parts of the galaxy and evade warring factions as well as authority agents out to get them. '
+  },
+
 ];
+
 
   return (
     <Router>
